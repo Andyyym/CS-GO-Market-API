bafda1975a8240f6b0c1a3c3e477384ef68a9282
Andyyy <42917091+Andyyym@users.noreply.github.com>
2023-01-11 09:51:53 +0200

    Caching implemented

--- API.js
+++ API.js
@@ -15,48 +15,97 @@ const limiter = rateLimit({
 
 app.use(limiter);
 
-app.get("/csgo-item/:itemName", (req, res) => {
-  const itemName = req.params.itemName;
-  const Itmes = [];
-  const currency = req.query.currency || "USD"; //defaulting to USD
-  request(Currency, function (error, response, body) {
-    const q = JSON.parse(body);
-    const getRand = q[currency];
+const cache = new Map();
+const CACHE_TIME = 3600 * 1000; // 1 hour in milliseconds
 
-    request(
-      `https://steamcommunity.com/market/search?appid=730&q=${itemName}`,
-      function (error, response, body) {
-        const $ = cheerio.load(body);
+const fetchItemsFromAPI = async (itemName, currency) => {
+  return new Promise((resolve, reject) => {
+    request(Currency, function (error, response, body) {
+      if (error) {
+        reject(error);
+      } else {
+        const q = JSON.parse(body);
+        const getRand = q[currency];
 
-        const getItems = $(
-          "div.market_listing_row.market_recent_listing_row.market_listing_searchresult"
+        request(
+          `https://steamcommunity.com/market/search?appid=730&q=${itemName}`,
+          function (error, response, body) {
+            if (error) {
+              reject(error);
+            } else {
+              const items = [];
+              const $ = cheerio.load(body);
+
+              const getItems = $(
+                "div.market_listing_row.market_recent_listing_row.market_listing_searchresult"
+              );
+
+              getItems.each(function () {
+                const name = $(this)
+                  .find("span.market_listing_item_name")
+                  .text();
+                const price = $(this).find("span.sale_price").text();
+                const img = $(this)
+                  .find("img.market_listing_item_img")
+                  .attr("src");
+                const listings = $(this)
+                  .find("span.market_listing_num_listings_qty")
+                  .text();
+
+                const newP = price.substring(1, price.length - 4);
+                const newF = parseFloat(newP);
+                const RoundedZar = Math.round(getRand * newF * 100) / 100;
+                const spaces = name.replace(" ", "%20");
+                const ItemSet = {
+                  Name: name,
+                  [currency]: RoundedZar,
+                  Listings: listings,
+                  BuffURL: Buff + spaces,
+                  Image: img,
+                };
+                items.push(ItemSet);
+              });
+              resolve(items);
+            }
+          }
         );
-
-        getItems.each(function () {
-          const name = $(this).find("span.market_listing_item_name").text();
-          const price = $(this).find("span.sale_price").text();
-          const img = $(this).find("img.market_listing_item_img").attr("src");
-          const listings = $(this).find("span.market_listing_num_listings_qty").text()
-
-          const newP = price.substring(1, price.length - 4);
-          const newF = parseFloat(newP);
-          const RoundedZar = Math.round(getRand * newF * 100) / 100;
-          const spaces = name.replace(" ", "%20");
-          const ItemSet = {
-            Name: name,
-            // USD: newF,
-            [currency]: RoundedZar,
-            Listings: listings,
-            BuffURL: Buff + spaces,
-            Image: img,
-          };
-          Itmes.push(ItemSet);
-        });
-
-        res.json(Itmes);
       }
-    );
+    });
   });
+};
+
+const getCacheData = (key) => {
+  if (cache.has(key)) {
+    const { value, timeoutId } = cache.get(key);
+    clearTimeout(timeoutId);
+    return value;
+  }
+  return null;
+};
+
+const setCacheData = (key, value) => {
+  const timeoutId = setTimeout(() => {
+    cache.delete(key);
+  }, CACHE_TIME);
+  cache.set(key, { value, timeoutId });
+};
+
+app.get("/csgo-item/:itemName", async (req, res) => {
+  const itemName = req.params.itemName;
+  const currency = req.query.currency || "USD"; //defaulting to USD
+
+  // Check if the result is already in the cache
+  const cacheData = getCacheData(`${itemName}${currency}`);
+  if (cacheData) {
+    res.json(cacheData);
+    return;
+  }
+
+  const items = await fetchItemsFromAPI(itemName, currency);
+  res.json(items);
+
+  // set the result to cache
+  setCacheData(`${itemName}${currency}`, items);
 });
 
 app.listen(3000, () => {
