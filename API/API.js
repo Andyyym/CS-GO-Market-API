754602452760ac3bb544b3bd5bbcf5d5308c6b53
Andyyy <42917091+Andyyym@users.noreply.github.com>
2023-01-24 12:31:15 +0200

    Minor bug fixes

--- API/API.js
+++ API/API.js
@@ -3,7 +3,8 @@ const request = require("request");
 const cheerio = require("cheerio");
 
 const Currency = "https://public-api.pricempire.com/api/meta/getCurrencyRates";
-const Buff = "https://api.pricempire.com/v1/redirectBuff/";
+const Buff = "https://buff.163.com/market/csgo#tab=selling&page_num=1&search=";
+const BaseURLSteam = 'https://steamcommunity.com/market/listings/730/'
 
 const fetchItemsFromAPI = async (itemName, currency, sort) => {
   return new Promise((resolve, reject) => {
@@ -24,29 +25,25 @@ const fetchItemsFromAPI = async (itemName, currency, sort) => {
               const $ = cheerio.load(body);
 
               const getItems = $(
-                "div.market_listing_row.market_recent_listing_row.market_listing_searchresult"
+                "div.market_listing_row"
               );
 
               getItems.each(function () {
-                const name = $(this)
-                  .find("span.market_listing_item_name")
-                  .text();
+                const name = $(this).find("span.market_listing_item_name").text();
                 const price = $(this).find("span.sale_price").text();
-                const img = $(this)
-                  .find("img.market_listing_item_img")
-                  .attr("src");
-                const listings = $(this)
-                  .find("span.market_listing_num_listings_qty")
-                  .text();
+                const img = $(this).find("img.market_listing_item_img").attr("src");
+                const listings = $(this).find("span.market_listing_num_listings_qty").text();
 
                 const newP = price.substring(1, price.length - 4);
                 const newF = parseFloat(newP);
                 const RoundedZar = Math.round(getRand * newF * 100) / 100;
-                const spaces = name.replace(" ", "%20");
+                const spaces = name.split(" ").join("%20");
+
                 const ItemSet = {
                   Name: name,
                   [currency]: RoundedZar,
                   Listings: listings,
+                  SteamURL: BaseURLSteam + spaces,
                   BuffURL: Buff + spaces,
                   Image: img,
                 };
